a59b7a5936dd93acb26dc196b74971010adaa9b5
Sam <samuel.wickstead@gmail.com>
2025-01-27 19:18:27 +0000

    initial commit

---
+++ src/client/java/io/github/samuelongithub/gdsmod/client/GdsmodClient.java
@@ -0,0 +1,10 @@
+package io.github.samuelongithub.gdsmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GdsmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/io/github/samuelongithub/gdsmod/Gdsmod.java
@@ -0,0 +1,10 @@
+package io.github.samuelongithub.gdsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gdsmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
